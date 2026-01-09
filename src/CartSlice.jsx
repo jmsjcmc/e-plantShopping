64ef877f834ade008fdb0c260aaee2ca28a1f5ba
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-01-09 16:01:49 +0800

    refactor: fix missing components

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,35 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+    const item = action.payload;
+    const existingItem = state.items.find(
+      (i) => i.name === item.name
+    ); 
+    if (existingItem){
+      existingItem.quantity += 1;
+    } else {
+      state.items.push({
+        ...item,
+        quantity: 1
+      })
+    }
     },
     removeItem: (state, action) => {
+      const name = action.payload;
+
+      state.items = state.items.filter(
+        (item) => item.name !== name
+      )
     },
     updateQuantity: (state, action) => {
+    const {name, amount} = action.payload;
+    const item = state.items.find(
+      (i) => i.name === name
+    );
 
-    
+    if (item){
+      item.quantity = amount;
+    }
     },
   },
 });
